ae2cfd246034bf8fe4cb7f0455aa42ab87939116
Alex Richardson <richardson.d.alex@gmail.com>
2018-10-25 14:04:23 -0500

    Added API Keys to gitignore

--- Dark Skies/ViewController.swift
+++ Dark Skies/ViewController.swift
@@ -7,11 +7,41 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let apikeys = APIKeys()
+        
+        let darkSkyURL = "https://api.darksky.net/forecast/"
+        
+        let darkSkyKey = apikeys.darkSkyKey
+        
+        let latitude = "37.8267"
+        
+        let longitude = "-122.4233"
+        
+        let url = darkSkyURL + darkSkyKey + "/" + latitude + "," + longitude
+        
+        let request = Alamofire.request(url)
+        
+        request.responseJSON { response in
+            switch response.result {
+                
+            case .success(let value):
+                
+                let json = JSON(value)
+                
+                print(json)
+                
+            case .failure(let error): print(error.localizedDescription)
+                
+            }
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
