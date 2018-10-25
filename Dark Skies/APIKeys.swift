1300e14869a283ea2d2441bd2008339f2b09aff4
Alex Richardson <richardson.d.alex@gmail.com>
2018-10-25 12:41:16 -0500

    Committing API Keys without the the actual values

---
+++ Dark Skies/APIKeys.swift
@@ -0,0 +1,17 @@
+//
+//  APIKeys.swift
+//  Dark Skies
+//
+//  Created by Alex Richardson on 10/25/18.
+//  Copyright © 2018 Alex Richardson. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys {
+    
+    let darkSkyKey = ""
+    
+    let googleKey = ""
+    
+}
